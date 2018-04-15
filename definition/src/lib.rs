8c33116d3b38186b4b4f990043584de0cb4d7142
Michał Krasnoborski <mkrdln@gmail.com>
2018-04-15 17:11:41 +0200

    go back to unimplemented

--- definition/src/lib.rs
+++ definition/src/lib.rs
@@ -5,7 +5,7 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 
 #[proc_macro]
-pub fn foo(input: TokenStream) -> TokenStream {
-    // unimplemented!()
-    input
+pub fn foo(_input: TokenStream) -> TokenStream {
+    unimplemented!()
+    // _input
 }
